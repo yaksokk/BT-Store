96483a275790f26096f1161fe299e7a47034c0a8
ssamueluntu <yakusoku751@gmail.com>
2023-12-09 22:36:40 +0700

    Membuat tampilan baru

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,35 +1,44 @@
-import React, {useState} from 'react';
-import { fontType, colors } from '../theme';
-import FastImage from 'react-native-fast-image';
-import {useNavigation} from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/FontAwesome5';
-import {StyleSheet, Text, View, FlatList,SafeAreaView, SectionList, TouchableOpacity} from 'react-native';
-import { ListOfItems } from '../../data';
+import {useNavigation} from '@react-navigation/native';
+import FastImage from 'react-native-fast-image';
+import {fontType, colors} from '../theme';
+import {ListOfItems} from '../../data';
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  SafeAreaView,
+  SectionList,
+  TouchableOpacity,
+} from 'react-native';
 
 const ContainerItem = ({item, onPress}) => {
   const navigation = useNavigation();
   return (
-    <TouchableOpacity style={listItems.cardItem} onPress={() => navigation.navigate('BlogDetail', {blogId: item.id})}>
+    <TouchableOpacity
+      style={listItems.cardItem}
+      onPress={() => navigation.navigate('BlogDetail', {blogId: item.id})}>
       <FastImage
         style={listItems.cardImage}
         source={{
-            uri: item.image,
-            headers: {Authorization: 'someAuthToken'},
-            priority: FastImage.priority.high,
-          }}
-          resizeMode={FastImage.resizeMode.cover}>
-      </FastImage>
-        <View style={listItems.cardContent}>
-          <View style={listItems.cardInfo}>
-            <Text style={listItems.cardTitle}>{item.name}</Text>
-            <Text style={listItems.cardText}>{item.price}</Text>
-          </View>
-          <View style={listItems.cardIcon}>
-              <TouchableOpacity onPress={onPress}>
-                  <Icon name='heart'  size={21} color={colors.black()} />
-              </TouchableOpacity>
-          </View>
+          uri: item.image,
+          headers: {Authorization: 'someAuthToken'},
+          priority: FastImage.priority.high,
+        }}
+        resizeMode={FastImage.resizeMode.cover}></FastImage>
+      <View style={listItems.cardContent}>
+        <View style={listItems.cardInfo}>
+          <Text style={listItems.cardTitle}>{item.name}</Text>
+          <Text style={listItems.cardText}>{item.price}</Text>
         </View>
+        <View style={listItems.cardIcon}>
+          <TouchableOpacity onPress={onPress}>
+            <Icon name="heart" size={21} color={colors.black()} />
+          </TouchableOpacity>
+        </View>
+      </View>
     </TouchableOpacity>
   );
 };
@@ -41,38 +50,39 @@ const ListItems = ({}) => {
     } else {
       setBookmark([...bookmark, itemId]);
     }
-  }
+  };
   return (
     <View>
-      <SafeAreaView style={{flex:1}}>
+      <SafeAreaView style={{flex: 1}}>
         <SectionList
-          contentContainerStyle={{paddingHorizontal:10}}
+          contentContainerStyle={{paddingHorizontal: 10}}
           stickySectionHeaderEnabled={false}
           sections={ListOfItems}
-          renderSectionHeader={({section}) =>(
+          renderSectionHeader={({section}) => (
             <>
-              <Text>{section.title}</Text>
+              <Text style={listItems.titleItem}>{section.title}</Text>
               {section.horizontal ? (
                 <FlatList
-                horizontal
-                data={section.data}
-                renderItem={({item}) => <ContainerItem item={item}/>}
-                showsHorizontalScrollIndicator={false}
+                  horizontal
+                  data={section.data}
+                  renderItem={({item}) => <ContainerItem item={item} />}
+                  showsHorizontalScrollIndicator={false}
                 />
-              ): null}
+              ) : null}
             </>
-            )}
-            renderItem = {({item, section}) => {
-              if(section.horizontal){
-                return null
-              }
-              return (
-                <ContainerItem
-                  item={item}
-                  onPress={() => toggleBookmark(item.id)}/>
-              )
-            }}
-          />
+          )}
+          renderItem={({item, section}) => {
+            if (section.horizontal) {
+              return null;
+            }
+            return (
+              <ContainerItem
+                item={item}
+                onPress={() => toggleBookmark(item.id)}
+              />
+            );
+          }}
+        />
       </SafeAreaView>
     </View>
   );
@@ -80,44 +90,48 @@ const ListItems = ({}) => {
 export default ListItems;
 
 const listItems = StyleSheet.create({
+  titleItem: {
+    fontSize: 17,
+    color: colors.blue(0.7),
+    fontFamily: fontType['pps-Medium'],
+  },
   cardItem: {
-      width: 170,
-      alignItems:'center',
-      backgroundColor:colors.black(.09),
-      borderRadius:7,
-      marginHorizontal:7,
-      marginVertical:14,
-      position:'relative',
-      paddingBottom:7
+    width: 170,
+    alignItems: 'center',
+    backgroundColor: colors.black(0.09),
+    borderRadius: 7,
+    marginHorizontal: 7,
+    marginVertical: 14,
+    position: 'relative',
+    paddingBottom: 7,
   },
   cardImage: {
-      width: 100,
-      height: 100,
-      borderRadius: 15,
-      marginVertical:14,
+    width: 100,
+    height: 100,
+    borderRadius: 15,
+    marginVertical: 14,
   },
   cardContent: {
-      flexDirection: 'row',
-      maxWidth:'100%',
+    flexDirection: 'row',
+    maxWidth: '100%',
   },
   cardInfo: {
-      width:'75%',
-      paddingHorizontal:7,
+    width: '75%',
+    paddingHorizontal: 7,
   },
   cardTitle: {
-      fontFamily: fontType['pps-Regular'],
-      fontSize: 14,
-      color: colors.black(),
-      flexWrap:'wrap'
+    fontFamily: fontType['pps-Regular'],
+    fontSize: 14,
+    color: colors.black(),
+    flexWrap: 'wrap',
   },
   cardText: {
-      fontSize: 10,
-      color: colors.black(),
-      fontFamily: fontType['pps-Regular'],
+    fontSize: 10,
+    color: colors.black(),
+    fontFamily: fontType['pps-Regular'],
   },
   cardIcon: {
-      width:'23%',
-      alignItems:'center'
+    width: '23%',
+    alignItems: 'center',
   },
-  
-})
+});
