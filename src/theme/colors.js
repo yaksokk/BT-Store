f4f96d1addf9d522f74aea86fee30677d4923f90
ssamueluntu <yakusoku751@gmail.com>
2023-12-10 00:00:31 +0700

    Mengubah warna biru

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -1,6 +1,6 @@
 const colors = {
     grey: (opacity = 1) => `rgba(136, 138, 164, ${opacity})`,
-    blue: (opacity = 1) => `rgba(53, 88, 225, ${opacity})`,
+    blue: (opacity = 1) => `rgba(0, 50, 225, ${opacity})`,
     white: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
     black: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
     darkModeBlack: (opacity = 1) => `rgba(27, 27, 27, ${opacity})`,
